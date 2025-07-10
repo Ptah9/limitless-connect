e27d4d0068e8d84cc0ae50db09dad1750c8f1e3c
Ptah9 <Ptah_9@icloud.com>
2025-07-11 01:54:27 +0300

    initialization

---
+++ web-app/script.js
@@ -0,0 +1,22 @@
+const inputForm = document.querySelector(".input-form"),
+      sendButton = document.querySelector(".send-button"),
+      textInput = document.querySelector(".text-input");
+
+
+
+inputForm.addEventListener('submit', function(event) {
+    let message = textInput.value
+    event.preventDefault();
+    textInput.value = "";
+    console.log(message)
+});
+
+
+
+fetch("http://localhost:3000/hello")
+  .then(res => res.json())
+  .then(data => {
+    console.log(data.message) // здесь будет "Hello"
+  })
+  .catch(err => console.error("Ошибка:", err))
+
